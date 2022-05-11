e79069c19e2a18b54a3cb9d34eee1860e4317acd
marco <marcofernandioo@gmail.com>
2022-05-11 15:37:47 +0800

    Initial commit with complete Views

---
+++ LogoutConfirmationView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace codebase
+{
+    public partial class LogoutConfirmationView : Form
+    {
+        public LogoutConfirmationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
